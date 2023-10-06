5d22d5de20f165065d1ee64074e025191af9f768
AbhishekBhure <abhiab7411@gmail.com>
2023-10-06 22:00:54 +0530

    added axios

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MdOutlineAddBox } from "react-icons/md";
 import { Link } from "react-router-dom";
 import BooksTable from "../components/home/BooksTable";
+import axios from "axios";
+import Spinner from "../components/Spinner";
 
 const Home = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showType, setShowType] = useState("table");
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("http://localhost:5555/books")
+      .then((response) => {
+        // console.log(response);
+        setBooks(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div className="p-4">
       <div className="flex justify-center items-center gap-x-4">
@@ -20,7 +41,7 @@ const Home = () => {
           <MdOutlineAddBox className="text-sky-800 text-4xl" />
         </Link>
       </div>
-      <BooksTable />
+      {loading ? <Spinner /> : <BooksTable books={books} />}
     </div>
   );
 };
