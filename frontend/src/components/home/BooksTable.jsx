87ad123811107cba01864d4d17d1a3dc53755614
AbhishekBhure <abhiab7411@gmail.com>
2023-10-06 21:57:23 +0530

    modified booksTable.jsx

--- frontend/src/components/home/BooksTable.jsx
+++ frontend/src/components/home/BooksTable.jsx
@@ -1,4 +1,10 @@
-const BooksTable = () => {
+import { AiOutlineEdit } from "react-icons/ai";
+import { MdOutlineDelete } from "react-icons/md";
+import { BsInfoCircle } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+const BooksTable = ({ books }) => {
+  console.log(books, "from booksTable");
   return (
     <table className="w-full border-separate border-spacing-2">
       <thead>
@@ -15,19 +21,37 @@ const BooksTable = () => {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td className="border border-slate-600 rounded-md text-center">1</td>
-          <td className="border border-slate-600 rounded-md text-center">
-            5am Club
-          </td>
-          <td className="border border-slate-600 rounded-md text-center max-md:hidden">
-            Robin Sharma
-          </td>
-          <td className="border border-slate-600 rounded-md text-center max-md:hidden">
-            2017
-          </td>
-          <td></td>
-        </tr>
+        {books.map((book, index) => (
+          <tr key={book._id}>
+            <td className="border border-slate-600 rounded-md text-center">
+              {index + 1}
+            </td>
+            <td className="border border-slate-600 rounded-md text-center">
+              {book.title}
+            </td>
+            <td className="border border-slate-600 rounded-md text-center max-md:hidden">
+              {book.author}
+            </td>
+            <td className="border border-slate-600 rounded-md text-center max-md:hidden">
+              {book.publishYear}
+            </td>
+            <td className="border border-slate-700 rounded-md text-center">
+              <div className="flex justify-center gap-x-4">
+                <Link to={`/books/details/${book._id}`}>
+                  <BsInfoCircle className="text-2xl text-green-800" />
+                </Link>
+
+                <Link to={`/books/edit/${book._id}`}>
+                  <AiOutlineEdit className="text-2xl text-yellow-800" />
+                </Link>
+
+                <Link to={`/books/delete/${book._id}`}>
+                  <MdOutlineDelete className="text-2xl text-red-800" />
+                </Link>
+              </div>
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
