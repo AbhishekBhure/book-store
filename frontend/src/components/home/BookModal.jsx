c8863eaddae64f3326cf35e083d1441637397583
AbhishekBhure <abhiab7411@gmail.com>
2023-10-12 18:09:12 +0530

    added BookModel component

---
+++ frontend/src/components/home/BookModal.jsx
@@ -0,0 +1,46 @@
+import { AiOutlineClose } from "react-icons/ai";
+import { PiBookOpenTextLight } from "react-icons/pi";
+import { BiUserCircle } from "react-icons/bi";
+
+const BookModal = ({ book, onClose }) => {
+  return (
+    <div
+      className="fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center"
+      onClick={onClose}
+    >
+      <div className="w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative">
+        <AiOutlineClose
+          className="absolute right-6 top-6 text-3xl text-red-600 cursor-pointer"
+          onClick={onClose}
+        />
+        <h2 className="w-fit px-4 py-1 bg-red-300 rounded-lg">
+          {" "}
+          {book.publishYear}{" "}
+        </h2>
+        <h4 className="my-2 text-gray-500">{book._id}</h4>
+        <div className="flex justify-center items-center gap-x-2">
+          <PiBookOpenTextLight className="text-red-300 text-2xl" />
+          <h2 className="my1"> {book.title} </h2>
+        </div>
+        <div className="flex justify-center items-center gap-x-2">
+          <BiUserCircle className="text-red-300 text-2xl" />
+          <h2 className="my1"> {book.author} </h2>
+        </div>
+        <p className="mt-4"> Anything You want show </p>
+        <p className="my2">
+          {" "}
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+          a aspernatur corporis dolorum esse quasi fugiat laudantium magnam?
+          Commodi officia labore doloribus? Dolores incidunt fugit atque porro
+          autem quibusdam minus! Blanditiis, provident ex! Ratione at recusandae
+          vitae maiores itaque unde voluptatibus consequuntur, ut dolorem
+          suscipit perspiciatis modi et, illo molestiae cupiditate vel.
+          Accusantium voluptatum natus architecto libero voluptatibus
+          consequuntur veritatis.{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BookModal;
