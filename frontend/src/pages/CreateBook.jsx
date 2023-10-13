f2ecfc4230cb7881d608d45d5d9306bc7470c9fb
AbhishekBhure <abhiab7411@gmail.com>
2023-10-13 22:21:57 +0530

    adding custom alert

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -3,6 +3,7 @@ import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 const CreateBook = () => {
   const [title, setTitle] = useState("");
@@ -10,6 +11,7 @@ const CreateBook = () => {
   const [publishYear, setPublishYear] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
 
   const handleSubmit = () => {
     const data = {
@@ -22,12 +24,14 @@ const CreateBook = () => {
       .post("http://localhost:5555/books", data)
       .then(() => {
         setLoading(false);
-        alert("Book is Created");
+        // alert("Book is Created");
+        enqueueSnackbar("Book Created successfully", { variant: "success" });
         navigate("/");
       })
       .catch((error) => {
         setLoading(false);
-        alert("An error occured");
+        // alert("An error occured");
+        enqueueSnackbar("Error while creating a Book", { variant: "error" });
         console.log(error);
       });
   };
